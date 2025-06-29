70ea5856a798f7cf02cf15a6bb311871025a5a29
meerathaakur <jayants2859@gmail.com>
2025-06-29 16:05:17 +0530

    notification

---
+++ client/src/contexts/NotificationContext.jsx
@@ -0,0 +1,28 @@
+// src/contexts/NotificationContext.js
+import { createContext, useState } from 'react';
+
+export const NotificationContext = createContext();
+
+export const NotificationProvider = ({ children }) => {
+  const [notifications, setNotifications] = useState([]);
+
+  const addNotification = (notification) => {
+    setNotifications((prev) => [...prev, notification]);
+  };
+
+  const removeNotification = (id) => {
+    setNotifications((prev) => prev.filter((n) => n.id !== id));
+  };
+
+  return (
+    <NotificationContext.Provider
+      value={{
+        notifications,
+        addNotification,
+        removeNotification
+      }}
+    >
+      {children}
+    </NotificationContext.Provider>
+  );
+};
