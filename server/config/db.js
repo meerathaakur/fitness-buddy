403dc14e4eac6bafc1e6820b74527e4210580cb5
meerathaakur <jayants2859@gmail.com>
2025-07-08 17:12:32 +0530

    DB connection URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
-const URI="mongodb://127.0.0.1:27017/fitness-buddy";
-// const URI = process.env.MONGO_URI 
+// const URI="mongodb://127.0.0.1:27017/fitness-buddy";
+const URI = process.env.MONGO_URI 
 
 const connectDB = async () => {
     try {
